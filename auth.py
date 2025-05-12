fd2822cefbf3ea9d7a0c6503b7b97ea0ed4d53f3
Riteshk1314 <riteshkapoor1314@gmail.com>
2025-05-12 20:43:45 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -9,7 +9,7 @@ from functools import wraps
 
 load_dotenv()
 
-JWT_SECRET = os.getenv("JWT_KEY")
+JWT_SECRET = os.getenv("JWT_SECRET")
 JWT_ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 15
 REFRESH_TOKEN_EXPIRE_DAYS = 7
